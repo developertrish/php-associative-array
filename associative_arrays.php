76c7de2052d63874f86b7724ba33784b1f8402b5
Trishin Rambajan <161632845+developertrish@users.noreply.github.com>
2024-02-29 00:10:40 +0200

    Create associative_arrays.php

---
+++ associative_arrays.php
@@ -0,0 +1,12 @@
+<?php
+	// Declare associative array and assign values to each key
+	$associative_array = [
+    	'name' => 'John',
+        'surname' => 'Doe',
+        'age' => 24,
+        'country' => 'United Kingdom'
+    ];
+    
+    // Replace 'name' with other array keys. Example: 'surname'
+    echo $associative_array['name'];
+?> 
